d93dba904e4aef02db17308ad783b58fbfd7c368
sagarmounika <sagarmouni5@gmail.com>
2021-09-20 18:13:22 +0530

    added solutions

---
+++ index.js
@@ -0,0 +1,85 @@
+function createArrayOfArrays(countries) {
+  return countries.map(ct => [
+    ct,
+    ct.substr(0, 3).toUpperCase(),
+    ct.length
+  ]);
+}
+
+const countries = [
+  'ALBANIA',
+  'BOLIVIA',
+  'CANADA',
+  'DENMARK',
+  'ETHIOPIA',
+  'FINLAND',
+  'GERMANY',
+  'HUNGARY',
+  'IRELAND',
+  'JAPAN',
+  'KENYA'
+];
+console.log(createArrayOfArrays(countries));
+
+const users = [
+  {
+    name: 'Brook',
+    scores: 75,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 16
+  },
+  {
+    name: 'Alex',
+    scores: 80,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 18
+  },
+  {
+    name: 'David',
+    scores: 75,
+    skills: ['HTM', 'CSS'],
+    age: 22
+  },
+  {
+    name: 'John',
+    scores: 85,
+    skills: ['HTM'],
+    age: 25
+  },
+  {
+    name: 'Sara',
+    scores: 95,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 26
+  },
+  {
+    name: 'Martha',
+    scores: 80,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 18
+  },
+  {
+    name: 'Thomas',
+    scores: 90,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 20
+  }
+];
+
+// 2(a)
+function scoresGreaterThan85(users) {
+  return users.filter(user => user.scores > 85);
+}
+console.log(scoresGreaterThan85(users))
+document.getElementById("app").innerHTML = `
+
+<div id="parent" >
+
+
+
+
+
+
+
+</div>
+`
